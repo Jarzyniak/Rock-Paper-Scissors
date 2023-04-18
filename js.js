2a964daaec2ad0dfdcb0aa7e3fde9da1c9dedeb6
Jarzyniak <108904336+Jarzyniak@users.noreply.github.com>
2023-04-19 00:04:32 +0200

    Added playerSelection

--- js.js
+++ js.js
@@ -1,17 +1,40 @@
+/* Computer choice function */
 function getComputerChoice() {
+    /*random number generator */
     let random = Math.floor((Math.random() * 3) + 1);
     let rps;
+    /* Assigning a value to a variable depends on the generated number */
     if (random === 1) {
-        rps = "rock";
+        rps === "rock";
     } else if (random === 2) {
-        rps = "paper";
+        rps === "paper";
     } else {
-        rps = "scissors"
+        rps === "scissors"
     }
-
+    /* return value */
     return rps;
 }
 
+/* My choice */
+function playerSelection() {
+    /*Write a value */
+    let selection = prompt("Let's play? What's your choice? ROCK/PAPER/SCISSORS?");
+    /* Made paramater case insensitive */
+    let insensitive = selection.toLowerCase();
+    /* If value is wrong return info */
+    if (insensitive !== 'rock' &&
+        insensitive !== 'paper' &&
+        insensitive !== 'scissors') {
+        console.log("It doesn't exist in this game");
+        /*if true return value */
+    } else {
+        return insensitive;
+    }
 
 
-console.log(getComputerChoice());
+    function game() {
+
+    }
+
+
+}
