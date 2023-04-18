8f452bdb88a264df9040ac83a49e8ff94b000286
Jarzyniak <108904336+Jarzyniak@users.noreply.github.com>
2023-04-18 23:00:43 +0200

    Computer choice function

--- js.js
+++ js.js
@@ -1,1 +1,17 @@
-console.log("Hello World");
+function getComputerChoice() {
+    let random = Math.floor((Math.random() * 3) + 1);
+    let rps;
+    if (random === 1) {
+        rps = "rock";
+    } else if (random === 2) {
+        rps = "paper";
+    } else {
+        rps = "scissors"
+    }
+
+    return rps;
+}
+
+
+
+console.log(getComputerChoice());
