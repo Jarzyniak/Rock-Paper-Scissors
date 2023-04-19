b98e0b39cae2e9da0e5ab34f6851975f8d2e46e0
Jarzyniak <108904336+Jarzyniak@users.noreply.github.com>
2023-04-19 22:44:27 +0200

    Play round function

--- js.js
+++ js.js
@@ -5,29 +5,52 @@ function getComputerChoice() {
     let rps;
     /* Assigning a value to a variable depends on the generated number */
     if (random === 1) {
-        rps === "rock";
+        rps = 'rock';
     } else if (random === 2) {
-        rps === "paper";
+        rps = 'paper';
     } else {
-        rps === "scissors"
+        rps = 'scissors';
     }
     /* return value */
     return rps;
 }
 
 /* My choice */
-function playerSelection() {
+function getPlayerSelection() {
     /*Write a value */
     let selection = prompt("Let's play? What's your choice? ROCK/PAPER/SCISSORS?");
     /* Made paramater case insensitive */
-    let insensitive = selection.toLowerCase();
+    selection = selection.toLowerCase();
     /* If value is wrong return info */
-    if (insensitive !== 'rock' &&
-        insensitive !== 'paper' &&
-        insensitive !== 'scissors') {
+    if (selection !== 'rock' &&
+        selection !== 'paper' &&
+        selection !== 'scissors') {
         console.log("It doesn't exist in this game");
         /*if true return value */
     } else {
-        return insensitive;
+        return selection;
     }
 }
+
+/*play round function */
+function playRound(playerSelection, computerSelection) {
+    /* if tie return tie */
+    if (playerSelection === computerSelection) {
+        return "tie";
+        /*if player win */
+    } else if ((playerSelection === "rock" && computerSelection === "paper") ||
+        (playerSelection === "paper" && computerSelection === "rock") ||
+        (playerSelection === "scissors" && computerSelection === "paper") ||
+        (playerSelection === "rock" && computerSelection === "scissors")) {
+        return "Player won " +
+            playerSelection + " beats " + computerSelection + "!";
+        /*else choices computer win */
+    } else {
+        return "Computer won" +
+            computerSelection + " beats " + playerSelection + "!";
+    }
+}
+
+const playerSelection = getPlayerSelection();
+const computerSelection = getComputerChoice();
+console.log(playRound(playerSelection, computerSelection));
